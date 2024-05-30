813b083e1ca3937a8ef0742f7adc8a128e8e496a
tejasathalye <me.tejasathalye@gmail.com>
2024-05-30 16:43:47 +0530

    Revert "Updated for deployment"
    This reverts commit d1f45aace3f2e375e3cc9a3ea6a5978e1064bf18.

---
+++ api/recommend.py
@@ -0,0 +1,53 @@
+import numpy as np
+import pandas as pd
+from flask import Flask, request
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import pickle
+
+app = Flask(__name__)
+
+# load the nlp model and tfidf vectorizer from disk
+filename = 'nlp_model.pkl'
+clf = pickle.load(open(filename, 'rb'))
+vectorizer = pickle.load(open('tranform.pkl', 'rb'))
+
+def create_similarity():
+    data = pd.read_csv('main_data.csv')
+    cv = CountVectorizer()
+    count_matrix = cv.fit_transform(data['comb'])
+    similarity = cosine_similarity(count_matrix)
+    return data, similarity
+
+def rcmd(m):
+    m = m.lower()
+    try:
+        data.head()
+        similarity.shape
+    except:
+        data, similarity = create_similarity()
+    if m not in data['movie_title'].unique():
+        return 'Sorry! The movie you requested is not in our database. Please check the spelling or try with some other movies'
+    else:
+        i = data.loc[data['movie_title'] == m].index[0]
+        lst = list(enumerate(similarity[i]))
+        lst = sorted(lst, key=lambda x: x[1], reverse=True)
+        lst = lst[1:11]
+        l = []
+        for i in range(len(lst)):
+            a = lst[i][0]
+            l.append(data['movie_title'][a])
+        return l
+
+@app.route("/recommend", methods=["POST"])
+def recommend():
+    movie = request.form['name']
+    rc = rcmd(movie)
+    if type(rc) == type('string'):
+        return rc
+    else:
+        m_str = "---".join(rc)
+        return m_str
+
+if __name__ == "__main__":
+    app.run()
